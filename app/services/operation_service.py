dd73f4411d7dd30c7c70993b081b7ab3d2079eef
DanilRozhin <danilrozhin2005@mail.ru>
2026-07-29 19:58:10 +0300

    feat, refactor: operation service (creating, getting, submitting)

--- app/services/operation_service.py
+++ app/services/operation_service.py
@@ -1,58 +1,50 @@
 from fastapi import status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database.enums import StatusType
 from app.exceptions import OperationError
 from app.repositories import OperationRepository
-from app.schemas import OperationRequest, OperationSingle
+from app.schemas import OperationCreate, OperationSingleResponse, OperationSubmitResponse
 
 
 class OperationService:
     def __init__(self, session: AsyncSession):
         self.operation_repo = OperationRepository(session=session)
 
-    async def create_operation(self, operation_request: OperationRequest) -> OperationSingle:
-        operation_id = operation_request.operationId
-        existing_operation = await self.operation_repo.get_operation_by_id(operation_id=operation_id)
-        if existing_operation is not None:
+    async def create_operation(self, operation_request: OperationCreate) -> OperationSingleResponse:
+        operation_response = await self.operation_repo.create_operation(operation_request=operation_request)
+        if operation_response is None:
             raise OperationError(
                 detail="Operation with this ID already exists",
                 status_code=status.HTTP_409_CONFLICT,
                 extra={
                     "service": "operation_service",
+                    "sub": operation_request.operationId,
+                },
+            )
+        return operation_response
+
+    async def get_operation_by_id(self, operation_id: str) -> OperationSingleResponse:
+        operation = await self.operation_repo.get_operation_by_id(operation_id=operation_id)
+        if operation is None:
+            raise OperationError(
+                detail="Operation with this ID not found",
+                status_code=status.HTTP_404_NOT_FOUND,
+                extra={
+                    "service": "operation_service",
                     "sub": operation_id,
                 },
             )
-        operation_response = await self.operation_repo.create_operation(
-            operation_request=operation_request.model_dump()
-        )
-        return operation_response
+        return operation
 
-    async def update_operation_status(self, operation_id: str, operation_status: StatusType) -> OperationSingle:
-        operation = await self.operation_repo.update_operation_status(
-            operation_id=operation_id,
-            operation_status=operation_status,
-        )
-        if operation is not None:
-            return operation
-        raise OperationError(
-            detail="Operation with this ID not found",
-            status_code=status.HTTP_404_NOT_FOUND,
-            extra={
-                "service": "operation_service",
-                "sub": operation_id,
-            },
-        )
-
-    async def get_operation_by_id(self, operation_id: str) -> OperationSingle:
-        operation = await self.operation_repo.get_operation_by_id(operation_id=operation_id)
-        if operation is not None:
-            return operation
-        raise OperationError(
-            detail="Operation with this ID not found",
-            status_code=status.HTTP_404_NOT_FOUND,
-            extra={
-                "service": "operation_service",
-                "sub": operation_id,
-            },
-        )
+    async def submit_operation(self, operation_id: str) -> OperationSubmitResponse:
+        response = await self.operation_repo.submit_operation(operation_id=operation_id)
+        if response.operation is None:
+            raise OperationError(
+                detail="Operation with this ID not found",
+                status_code=status.HTTP_404_NOT_FOUND,
+                extra={
+                    "service": "operation_service",
+                    "sub": operation_id,
+                },
+            )
+        return response
