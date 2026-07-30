1929f66ecc4e232eb5fcc647963c683e48e0b18e
DanilRozhin <danilrozhin2005@mail.ru>
2026-07-30 13:54:43 +0300

    feat: receipt outcome enum added

--- app/database/enums/__init__.py
+++ app/database/enums/__init__.py
@@ -1,6 +1,7 @@
 from .attempt_outcome_enum import AttemptOutcomeType
 from .currency_enum import CurrencyType
 from .event_enum import EventType
+from .receipt_outcome_enum import ReceiptOutcome
 from .receipt_result_enum import ReceiptResultType
 from .status_enum import StatusType
 from .trigger_enum import TriggerType
@@ -9,6 +10,7 @@ __all__ = (
     "AttemptOutcomeType",
     "CurrencyType",
     "EventType",
+    "ReceiptOutcome",
     "ReceiptResultType",
     "StatusType",
     "TriggerType",
