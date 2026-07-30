8b73dfaaae92c58139809038b5328834bf03ea16
DanilRozhin <danilrozhin2005@mail.ru>
2026-07-31 01:21:23 +0300

    feat: get operation for provider call function added

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,9 +1,12 @@
 from .event_service import EventService
 from .operation_service import OperationService
+from .provider_call_service import ProviderCallService, run_provider_call_in_background
 from .receipt_service import ReceiptService
 
 __all__ = (
     "EventService",
     "OperationService",
+    "ProviderCallService",
     "ReceiptService",
+    "run_provider_call_in_background",
 )
