285a6c293128d0b9ce3545466e89ca5a3f53be37
DanilRozhin <danilrozhin2005@mail.ru>
2026-07-30 13:58:05 +0300

    feat: receipt repository created

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,4 +1,9 @@
 from .event_repository import EventRepository
 from .operation_repository import OperationRepository
+from .receipt_repository import ReceiptRepository
 
-__all__ = ("EventRepository", "OperationRepository")
+__all__ = (
+    "EventRepository",
+    "OperationRepository",
+    "ReceiptRepository",
+)
