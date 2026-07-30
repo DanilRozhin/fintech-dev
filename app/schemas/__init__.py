af1444e03fefc35f151b02c73d74b13a05cee71c
DanilRozhin <danilrozhin2005@mail.ru>
2026-07-30 13:57:00 +0300

    refactor, feat: receipt schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,7 +2,7 @@ from .event_schemas import OperationEventResponse, OperationEventSingle
 from .health_schemas import HealthResponse
 from .operation_schemas import OperationCreate, OperationSingleResponse, OperationSubmitResponse
 from .provider_attempt_schemas import AttemptSingle
-from .receipt_record_schemas import ReceiptSingle
+from .receipt_schemas import ReceiptRequest, ReceiptSingle
 
 __all__ = (
     "AttemptSingle",
@@ -12,5 +12,6 @@ __all__ = (
     "OperationEventSingle",
     "OperationSingleResponse",
     "OperationSubmitResponse",
+    "ReceiptRequest",
     "ReceiptSingle",
 )
